91f9ca2fd4c123f503bb46d403c8156ae1de7d08
RedSnicker (@Absurd_y) <115105740+Economic1024@users.noreply.github.com>
2024-07-11 02:14:17 +0330

    Update CitizensHook.java

--- duels-plugin/src/main/java/me/realized/duels/hook/hooks/CitizensHook.java
+++ duels-plugin/src/main/java/me/realized/duels/hook/hooks/CitizensHook.java
@@ -4,6 +4,7 @@ package me.realized.duels.hook.hooks;
 import me.realized.duels.DuelsPlugin;
 import me.realized.duels.config.Config;
 import me.realized.duels.util.hook.PluginHook;
+import net.citizensnpcs.*;
 
 public class CitizensHook extends PluginHook<DuelsPlugin> {
 
@@ -13,7 +14,8 @@ public class CitizensHook extends PluginHook<DuelsPlugin> {
 
     public CitizensHook(final DuelsPlugin plugin) {
         super(plugin, NAME);
-        this.config = plugin.getConfiguration();
+        this.config = plugin.getConfiguration(); //error is normal dont touch it
+
     }
 }
 /*?}*/
